02ba4820c13d8df76e0edec3f8d248be259f87ba
profLewis <plewis@lewisair.home>
2016-01-18 08:45:01 +0000

    updated from leaf

--- setup.py
+++ setup.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python
-"""Setup script for building prosail's python bindings"""
+"""Setup script for building leaf's python bindings"""
 
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration(parent_package,top_path)
-    config.add_extension('prosail_fortran', ['prosail/dataSpec_P5B.f90', \
-        "prosail/dladgen.f", "prosail/LIDF.f90", "prosail/MODULE_PRO4SAIL.f90", \
-        "prosail/PRO4SAIL.f90", "prosail/prospect_5B.f90", \
-        "prosail/run_prosail.f90","prosail/tav_abs.f90", \
-        "prosail/volscatt.f90","prosail/prosail_fortran.pyf"] )
+    config.add_extension('leaf', ['leaf/dataSpec_P5B.f90', \
+        leaf/getdata.py","leaf/__init__.py"] )
     return config
 
 if __name__ == "__main__":
     from numpy.distutils.core import setup
     # Global variables for this extension:
-    name         = "prosail"  # name of the generated python extension (.so)
-    description  = "PRO4SAIL python wrappers"
-    long_description = "The PROSPECT + SAILh radiative transfer models from Python."
-    author       = "J Gomez-Dans/NCEO & University College London"
-    author_email = "j.gomez-dans@ucl.ac.uk"
-    url = "http://github.com/jgomezdans/prosail"
+    name         = "leaf"  # name of the generated python extension (.so)
+    description  = "PROSPECT-based leaf scattering method"
+    long_description = "The PROSPECT RT model with flexible inputs."
+    author       = "Prof. P. Lewis/NCEO & University College London"
+    author_email = "p.lewis@ucl.ac.uk"
+    url = "https://github.com/UCL-EO/leaf.git"
     
     setup( name=name,\
         description=description, \
         author=author, \
         author_email = author_email, \
-        configuration = configuration, version="1.1.3",\
-        packages=["prosail"])
+        configuration = configuration, version="1.0.0",\
+        packages=["leaf"])
