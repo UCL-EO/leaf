56a88cd106f13a22b5903a1bf0483d1a2dd1ee39
Professor Philip Lewis <dr.p.e.lewis@gmail.com>
2016-01-18 14:24:31 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration(parent_package,top_path)
     config.add_extension('leaf', ['leaf/dataSpec_P5B.f90', \
-        leaf/getdata.py","leaf/__init__.py"] )
+        "leaf/getdata.py","leaf/__init__.py"] )
     return config
 
 if __name__ == "__main__":
